b9a9a04124555a2f97713e1495f64eb4908fd0f8
Justin McMurdie <mcmurdiej@gmail.com>
2017-12-12 15:55:44 -0700

    Spaces not tabs

--- src/react-disposable-decorator.js
+++ src/react-disposable-decorator.js
@@ -4,40 +4,40 @@ const inTestingEnv = typeof jasmine !== 'undefined';
 const enabled = !inTestingEnv || (typeof ReactDisposableDecoratorEnabled !== 'undefined' && ReactDisposableDecoratorEnabled);
 
 export default function(DecoratedComponent) {
-	if (!enabled) {
-		return DecoratedComponent;
-	}
+  if (!enabled) {
+    return DecoratedComponent;
+  }
 
-	return class Disposable extends React.Component {
+  return class Disposable extends React.Component {
     static displayName = `Disposable(${DecoratedComponent.displayName || DecoratedComponent.name})`
 
-		constructor(props) {
-			super(props);
-			this.disposables = [];
-		}
+    constructor(props) {
+      super(props);
+      this.disposables = [];
+    }
 
-		render() {
-			return <DecoratedComponent {...this.props} cancelWhenUnmounted={this.cancelWhenUnmounted} cancelAllSubscriptions={this.cancelAllSubscriptions} />
-		}
-		componentWillUnmount() {
-			this.cancelAllSubscriptions();
-		}
-		cancelWhenUnmounted = (...thingsToCancel) => {
-			thingsToCancel.forEach(thingToCancel => {
-				if (!thingToCancel || typeof thingToCancel.dispose !== 'function') {
-					throw new Error(`cancelWhenUnmounted should be called with one or more disposables (an object with a dispose function)`);
-				}
-				this.disposables.push(thingToCancel);
-			});
-		}
-		cancelAllSubscriptions = () => {
-			this.disposables.forEach(disposable => {
-				if (disposable && typeof disposable.dispose === 'function') {
-					disposable.dispose();
-				}
-			});
+    render() {
+      return <DecoratedComponent {...this.props} cancelWhenUnmounted={this.cancelWhenUnmounted} cancelAllSubscriptions={this.cancelAllSubscriptions} />
+    }
+    componentWillUnmount() {
+      this.cancelAllSubscriptions();
+    }
+    cancelWhenUnmounted = (...thingsToCancel) => {
+      thingsToCancel.forEach(thingToCancel => {
+        if (!thingToCancel || typeof thingToCancel.dispose !== 'function') {
+          throw new Error(`cancelWhenUnmounted should be called with one or more disposables (an object with a dispose function)`);
+        }
+        this.disposables.push(thingToCancel);
+      });
+    }
+    cancelAllSubscriptions = () => {
+      this.disposables.forEach(disposable => {
+        if (disposable && typeof disposable.dispose === 'function') {
+          disposable.dispose();
+        }
+      });
 
-			this.disposables = [];
-		}
-	}
+      this.disposables = [];
+    }
+  }
 }
