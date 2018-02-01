6cbe634d09a90881b04f7a9750ca27e7bf243a60
Joel Denning <joel.denning@canopytax.com>
2018-02-01 12:07:22 -0700

    Being smarter about displayName

--- src/react-disposable-decorator.js
+++ src/react-disposable-decorator.js
@@ -1,16 +1,19 @@
 import React from "react";
 
 const disabled = typeof disableReactDisposableDecorator === 'undefined' ? false : disableReactDisposableDecorator;
+const inTestingEnv = typeof jasmine !== 'undefined';
 
 export default function(DecoratedComponent) {
   if (disabled) {
     return DecoratedComponent;
   }
 
+  const originalDisplayName = DecoratedComponent.displayName || DecoratedComponent.name;
+
   return class Disposable extends React.Component {
-    // Don't change display name so snapshots and wrapper.find('CompName') works.
-    // Unfortunately, react-dev-tools might be weird though with two components with same name
-    static displayName = DecoratedComponent.displayName || DecoratedComponent.name
+    // Don't change display name in tests, so that snapshots and wrapper.find('CompName') works.
+    // But in browser/non-tests react-dev-tools are easier to understand with a slightly altered display name
+    static displayName = inTestingEnv ? originalDisplayName : `Cancelable(${originalDisplayName})`
 
     constructor(props) {
       super(props);
